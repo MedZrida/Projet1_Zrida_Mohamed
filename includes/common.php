b1a905519c6ba7cde6c85ea0acb53ed40ca2022b
Mohamed Zrida <zridamohamed@96@gmail.com>
2023-12-20 17:52:19 -0600

    init

---
+++ includes/common.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect("localhost","root","","ecom1_project");
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;ZRIDA CONCEPT | All Rights Reserved | Contact Us: +X XXX XXX XXXX</b></span></div>
+</footer>
+    
